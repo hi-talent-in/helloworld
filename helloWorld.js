2b9a28419a263a553d7e9247a1fe8079d3fa476a
visionaryM <mpsk427@gmail.com>
2021-12-04 11:06:43 +0530

    Initiating api helloworld

---
+++ helloWorld.js
@@ -0,0 +1,11 @@
+let express = require('express');
+
+let app = express();
+
+app.get('/api/helloworld', function(req, res){
+	res.send("Hello World!");
+});
+
+app.listen(8080, function(){
+	console.log("app listening...");
+});
