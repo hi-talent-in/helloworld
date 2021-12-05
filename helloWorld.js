3d94e9e6ff80b55f72c4b3e5dd0f6faf74384f54
visionaryM <mpsk427@gmail.com>
2021-12-05 10:00:55 +0530

    Dockerfile Stage 2 username added

--- helloWorld.js
+++ helloWorld.js
@@ -1,10 +1,21 @@
 "use strict";
 
 let express = require('express');
+let url = require('url');
 let app = express();
 
 app.get('/api/helloworld', function(req, res){
-		res.send("Hello World!");
+	const queryData = url.parse(req.url, true).query;
+	console.log(queryData.name);
+	if (queryData.name != undefined){
+	
+		let msg = "Hello " + queryData.name;
+		res.write(msg);
+	}
+	else{
+		res.write("Hello World!");
+	}
+	res.send();
 });
 
 app.listen(8080, function(){
