35a99126318c24f55fdb3f059b0d9babc64a64ae
Renuka <renukawaghmare358@gmail.com>
2021-12-07 16:44:32 +0530

    Basic Hello World in .Net Core

---
+++ HelloWorldProject/HelloWorldProject/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet(Name = "GetString")]
+
+        public String GET()
+        {
+            return "Hello World";
+
+        }
+    }
+}
