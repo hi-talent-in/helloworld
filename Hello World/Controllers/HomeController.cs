abeafa92067d331860c50baa135c3e583102300f
unknown <gpb5496@gmail.com>
2021-12-07 16:14:35 +0530

    First commitment Done

---
+++ Hello World/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Hello_World.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Hello_World.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        
+    }
+}
