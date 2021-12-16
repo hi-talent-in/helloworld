040016a44bcf9acba63ec849f97b9e9bbe746c29
fareen341 <59610617+fareen341@users.noreply.github.com>
2021-12-16 14:59:02 +0530

    changed GetApi

--- demoproject2/demoapp2/views.py
+++ demoproject2/demoapp2/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def GetApi(request):
-    return HttpResponse ("<h1>Django Application</h1>")
+    return HttpResponse ("<h1>Hello World!!!</h1>")
