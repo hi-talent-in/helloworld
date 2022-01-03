075cf2ed440db56d5c4400d3c8de0259a90231c4
Suraj Sharma <88436237+suraj-sh@users.noreply.github.com>
2022-01-03 11:07:30 +0530

    Update settings.py

--- testapi/settings.py
+++ testapi/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = '^s2$*=_1f44m040%e2ii1%%6o1*vka9&=$*icr&a29=ckwe+nq'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
