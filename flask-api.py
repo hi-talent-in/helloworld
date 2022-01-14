a2abef5e7a31cf425fe4fc5bd0a3b2fa8c564757
iliyas01 <shaikmohammediliyas837@gmail.com>
2022-01-14 12:33:57 +0530

    changes

--- flask-api.py
+++ flask-api.py
@@ -1,9 +1,10 @@
 from flask import Flask
 app=Flask(__name__)
 
-@app.route('/')
-def api():
-    return "Hello world"
+@app.route('/',)
+def index():
+    return "Hello world!"
 
 if __name__== "__main__" :
+    app.debug=False
     app.run()
