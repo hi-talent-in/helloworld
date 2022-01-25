62c1fe2db28d7389e3503d7bc32f34258101b30b
iliyas01 <97572380+iliyas01@users.noreply.github.com>
2022-01-25 08:07:14 +0530

    heroku-app deleted

--- heroku-app/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-@app.route('/')
-def iliyas():
-    return "HELLO WORLD!"
-if __name__ == '__main__':
-    
-    app.run(host='0.0.0.0', debug=False)
