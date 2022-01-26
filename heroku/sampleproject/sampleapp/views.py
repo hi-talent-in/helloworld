6a8250028913c9157d94b37b1a43b419da359445
iliyas01 <shaikmohammediliyas837@gmail.com>
2022-01-26 11:39:55 +0530

    flask-heroku

--- heroku/sampleproject/sampleapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-def search(request):
-    if 'name' in request.GET.keys():
-        message = 'Hello %r' % request.GET['name']
-    else:
-        message = 'Hello World'
-    return HttpResponse(message)
