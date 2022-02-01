b2bc60f6bdeb66ece791c686eca88e91d25424e3
Ashok Khandhar <ashokkhandhar18@gmail.com>
2022-02-01 11:17:41 +0530

    Basic hello world API with parameter name

--- workdone/app.js
+++ workdone/app.js
@@ -1,9 +1,12 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const port = 3000;
 
-const server = http.createServer(function (req, res) {  
-    res.end('hello world!');
-})
-server.listen(port);
-  
+app.get("/", (req, res) => {
+    if (req.query.name==undefined) res.send("Hello world!");
+    else res.send("heloo " + req.query.name + "!");
+});
+
+app.listen(port);
+
 console.log('Server running at: ' + port);
