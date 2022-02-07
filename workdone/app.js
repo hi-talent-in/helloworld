4ef9d98239280b0c1ac8d5461349a55ff08aa917
Ashok Khandhar <ashokkhandhar18@gmail.com>
2022-02-07 19:49:12 +0530

    Dockerized

--- workdone/app.js
+++ workdone/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
+
 const port = 3000;
 
-app.get("/", (req, res) => {
-    if (req.query.name==undefined) res.send("Hello world!");
-    else res.send("heloo " + req.query.name + "!");
-});
+app.get('/', (req, res)=>{
+    let name = req.query['name'];
+    if(name) {
+        res.send("Heloo " + name + "!");
+        console.log("parameter name: " + name + " entered.");
+    }
+    else {
+        res.send("Hello world!");
+        console.log("parameter name is empty!");
+    }
+})
 
-app.listen(port);
-
-console.log('Server running at: ' + port);
+app.listen(port, ()=>{
+    console.log('Server is live at port: ' + port);
+})
