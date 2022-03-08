9cf6babb4e2bb3fe70f35a1cc9d2c3e8854dd9c1
anurag438 <davidanurag6@gmail.com>
2022-03-08 12:22:33 +0530

    Basic hello world API with parameter name

--- level1/app.js
+++ level1/app.js
@@ -1,14 +1,22 @@
-const http = require('http');
-
+import { createServer } from 'http';
 const hostname = '127.0.0.1';
 const port = 5500;
+import { parse } from 'url';
+const server = createServer((req, res) => {
+  const queryObject = parse(req.url,true).query;
 
-const server = http.createServer((req, res) => {
+  console.log(queryObject.name);
+  let name = queryObject.name
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello world !');
+  if(name){
+    res.end(`Hello ${name}`)
+  }
+  else
+    res.end('Hello  world!');
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  
 });
