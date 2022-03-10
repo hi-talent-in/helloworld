bce61efb935ffc2ac1b4fa075cf50885886734c2
Ashok Khandhar <ashokkhandhar18@gmail.com>
2022-03-10 19:08:26 +0530

    Introduced Cache

--- workdone/app.js
+++ workdone/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const mysql = require('mysql');
+const redis = require('redis');
 
+// create client
+const redisClient = redis.createClient({
+    host: 'test-redis',
+    port: 6379
+  });
 
 // Create connection
 const db = mysql.createConnection({
@@ -19,8 +25,7 @@ db.connect((err) => {
 // create database
 const database = 'CREATE DATABASE IF NOT EXISTS records';
 // create table
-const table = 'CREATE TABLE IF NOT EXISTS Logs(id int AUTO_INCREMENT, name VARCHAR(50), count int, PRIMARY KEY(id))';
-console.log("working with docker-compose to use database with app");
+const table = 'CREATE TABLE IF NOT EXISTS Logs(id int AUTO_INCREMENT, name VARCHAR(50), count int, PRIMARY name(id))';
 
 let insert = 'INSERT INTO Logs(name,count) values (?,?)';
 let update = 'update Logs set count = count+1 where name=?';
@@ -29,35 +34,44 @@ let getlogs = 'SELECT * FROM Logs WHERE name = ?';
 const app = express();
 
 app.get('/', (req, res) => {
-    let name = req.query['name'];
-    if (name) {
-        res.send(`Hello ${name}!`);
-
-        // check wether it already exeiest or not
-        db.query(getlogs,[name], (err, result) => {
-            if (err) throw err;
-            console.log(result);
-            if (result.length>0) {
-                db.query(update, [name], (err, result) => {
-                    if (err) throw err;
-                    else console.log("log is already exiest, count updated")
-                });
-            }
-            else {
-                db.query(insert, [name, 1], (err, result) => {
-                    if (err) throw err;
-                    else console.log("It's new log, added to database");
-                });
-            }
-        });
-    }
-    else {
-        res.send("Hello world!");
-        console.log("parameter name is empty!");
-    }
+    const name = req.query['name'];
+    redisClient.get(name, (err, counts) => {
+        const countsTodisp = parseInt(counts)+1;
+        if(isNaN(countsTodisp)) {
+            db.query(getlogs,[name], (err, result)=>{
+                if(err) throw err;
+                console.log(result);
+                if(result.length>0){
+                    redisClient.set(name,result[0].count+1);
+                    db.query(update,[name], (err,result)=>{
+                        if(err) throw err;
+                        else console.log("log is already exiest in database, count updated");
+                    });
+                    res.send(`Hello ${name}! log is already exiest in database and redis, count updated `);
+                }
+                else{
+                    redisClient.set(name,1);
+                    db.query(insert,[name,1], (err,result)=>{
+                        if(err) throw err;
+                        else console.log("It's new log in database, added to database");
+                    });
+                    res.send(`Hello ${name}! It's new log in database and redis`)
+                }
+            });
+        }
+        else{
+            res.send(`Hello ${name}! ${countsTodisp} log is already exiest in redis`);
+            counts++;
+            redisClient.set(name,counts);
+            db.query(update,[name], (err,result)=>{
+                if(err) throw err;
+                else console.log("log is already exiest in database, count updated");
+            });       
+        }
+    });
 });
 
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is live at port: ${port}`);
-})
+});
