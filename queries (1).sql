52ac105131cc0e42a6f7ac5177d63ebbaa87b7da
meghabendre <meghabendre21@gmail.com>
2022-04-04 21:07:26 +0530

    task complete

---
+++ queries (1).sql
@@ -0,0 +1,13 @@
+
+
+CREATE TABLE student (
+  studid INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  dept TEXT NOT NULL
+);
+
+
+INSERT INTO student VALUES (1,   'MEGHA',   'CSE');
+
+
+SELECT * FROM student;
